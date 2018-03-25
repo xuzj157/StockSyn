916aff796709f65be6e168ebe39a5a52308077f1
xuzj157 <xuzj157@sina.com>
2018-03-25 13:39:34 +0800

    重新创建项目

---
+++ src/main/java/personal/xuzj157/stocksyn/StockSynApplication.java
@@ -0,0 +1,12 @@
+package personal.xuzj157.stocksyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSynApplication.class, args);
+	}
+}
---
+++ src/test/java/personal/xuzj157/stocksyn/StockSynApplicationTests.java
@@ -0,0 +1,16 @@
+package personal.xuzj157.stocksyn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockSynApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
